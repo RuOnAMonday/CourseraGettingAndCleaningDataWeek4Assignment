6a4332e61d87737cf8401eb7e74caa576d120cc3
Ru <RuOnAMonday@users.noreply.github.com>
2017-11-07 00:41:41 +1100

    Update run_analysis.R

--- run_analysis.R
+++ run_analysis.R
@@ -2,28 +2,28 @@
 
 # Reading datasets
 x_test <- read.table("./UCI HAR Dataset/test/X_test.txt") 
-View(x_test)
-dim(x_test)
+#View(x_test)
+#dim(x_test)
 
 y_test <- read.table("./UCI HAR Dataset/test/y_test.txt") 
-View(y_test)
-dim(y_test)
+#View(y_test)
+#dim(y_test)
 
 subject_test <- read.table("./UCI HAR Dataset/test/subject_test.txt") 
-View(subject_test)
-dim(subject_test)
+#View(subject_test)
+#dim(subject_test)
 
 x_train <- read.table("./UCI HAR Dataset/train/X_train.txt") 
-View(x_train)
-dim(x_train)
+#View(x_train)
+#dim(x_train)
 
 y_train <- read.table("./UCI HAR Dataset/train/y_train.txt") 
-View(y_train)
-dim(y_train)
+#View(y_train)
+#dim(y_train)
 
 subject_train <- read.table("./UCI HAR Dataset/train/subject_train.txt") 
-View(subject_train)
-dim(subject_train)
+#View(subject_train)
+#dim(subject_train)
 
 # Merging training and test data sets to create combined data sets 
 y_tt <- rbind(y_train,y_test)
@@ -32,20 +32,20 @@ subject_tt <- rbind(subject_train,subject_test)
 all_data <- cbind(x_tt,y_tt,subject_tt)
 
 # Viewing final merged dataset
-View(all_data)
+#View(all_data)
 
 
 #2.
 # Read the features table
 features <- read.table("./UCI HAR Dataset/features.txt") 
-View(features)
-dim(features)
+#View(features)
+#dim(features)
 
 # The first column of features table corresponds to the column positions of x_tt (combined training and test datasets for x)
 
 # Find all features with mean and standard deviation
 meanstdfeatures <- features[grepl("mean\\(\\)|std\\(\\)",features$V2),]
-View(meanstdfeatures)
+#View(meanstdfeatures)
 
 # Map entries with mean and standard deviation to the main x_tt table (which is the rbind between test and train datasets)
 # Use the first column of meanstdfeatures to match the columns numbers of x_tt
@@ -54,11 +54,11 @@ x_tt_meanstd <- x_tt[,meanstdfeatures[,1]]
 # update the column names to be descriptive too
 colnames(x_tt_meanstd) <- meanstdfeatures[,2]
 
-View(x_tt_meanstd)
+#View(x_tt_meanstd)
 
 # Bind this subset with named columns for x, with y and subject
 all_data <- cbind(x_tt_meanstd,y_tt,subject_tt)
-View(all_data)
+#View(all_data)
 # Now the x part of the data set is named properly
 
 #3
@@ -69,16 +69,16 @@ colnames(all_data)[68] <- "Subject"
 
 # Reading the activity labels file
 activity_labels <- read.table("./UCI HAR Dataset/activity_labels.txt") 
-View(activity_labels)
+#View(activity_labels)
 
 # Merging with activity labels so that the activity column has descriptive names 
 data_with_activity <- merge(all_data, activity_labels, by.x = "V1", by.y = "V1", sort = FALSE)
-View(data_with_activity)
+#View(data_with_activity)
 
 # Now that we have a descriptive activity column from the merge, we can get rid of the V1 column
 # V1 is the non descriptive version of V2
 all_data <-data_with_activity[,2:69]
-View(all_data)
+#View(all_data)
 
 #4
 # Using the features definition to rename the column names in x
@@ -91,7 +91,7 @@ colnames(all_data)[67] <- "Subject"
 
 # Giving descriptive name for Activity Column
 colnames(all_data)[68] <- "ActivityName"
-View(all_data)
+#View(all_data)
 
 
 #5
@@ -101,6 +101,6 @@ library(plyr)
 
 averages_data <- ddply(all_data, .(ActivityName,Subject), function(x) colMeans(x[, 1:66]))
 
-View(averages_data)
+#View(averages_data)
 
 write.table(averages_data, "./UCI HAR Dataset/tidydata.txt", sep="\t", row.names = FALSE, col.names = TRUE)
